c27b557679ed971bde2f6294bd04686f0cb97231
oceannguyen190196 <124870172+oceannguyen190196@users.noreply.github.com>
2023-02-09 15:16:40 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -12,8 +12,8 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = config_yaml["6218475791:AAFH0WdVkZaHZZGbITR0v5DFFgo7w_xH4v0"]
+openai_api_key = config_yaml["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"]
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
